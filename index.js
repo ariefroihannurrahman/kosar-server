1cb1fd141dac2591a255f7a1026a4c67eead922f
Arief Roihan Nur Rahman <ariefroihannurrahman1@gmail.com>
2023-05-19 23:33:51 +0700

    Change server.js into index.js

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 /* eslint linebreak-style: ["error", "windows"] */
 
-import init from './src/server.js';
+import Hapi from '@hapi/hapi';
+import routes from './src/routes.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const config = {
+  // port: process.env.NODE_PORT,
+  // host: process.env.NODE_HOST,
+  port: 3000,
+//   host: 'localhost',
+  host: 'kosar-server-k9kj3qadw-ariefroihannurrahman1-gmailcom.vercel.app',
+};
+
+const init = async () => {
+  const server = Hapi.server(config);
+  server.route(routes);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
 
 init();
--- src/database.js
+++ src/database.js
@@ -5,11 +5,16 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const connection = MySQL.createConnection({
-  host: process.env.NODE_DATABASE_HOST,
-  port: process.env.NODE_DATABASE_PORT,
-  user: process.env.NODE_DATABASE_USER,
-  password: process.env.NODE_DATABASE_PASSWORD,
-  database: process.env.NODE_DATABASE_DATABASE,
+  // host: process.env.NODE_DATABASE_HOST,
+  // port: process.env.NODE_DATABASE_PORT,
+  // user: process.env.NODE_DATABASE_USER,
+  // password: process.env.NODE_DATABASE_PASSWORD,
+  // database: process.env.NODE_DATABASE_DATABASE,
+  host: 'aqf.h.filess.io',
+  port: 3307,
+  user: 'KosarServer_blowoccur',
+  password: '14c04729b58b831683bf657f867a69a6ce8f7092',
+  database: 'KosarServer_blowoccur',
 });
 
 connection.connect((error) => {
