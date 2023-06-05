9aa4074076e9bdefc3bb36421660223ac2549b55
Arief Roihan Nur Rahman <ariefroihannurrahman1@gmail.com>
2023-06-05 09:47:11 +0700

    Add Cors

--- index.js
+++ index.js
@@ -3,15 +3,23 @@
 import Hapi from '@hapi/hapi';
 import routes from './src/routes.js';
 import * as dotenv from 'dotenv';
+import hapiCors from 'hapi-cors';
 
 dotenv.config();
 const config = {
   port: process.env.NODE_PORT,
-  host: process.env.NODE_HOST
+  host: process.env.NODE_HOST,
 };
 
 const init = async () => {
   const server = Hapi.server(config);
+  await server.register({
+    plugin: hapiCors,
+    options: {
+      origins: ['*'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    },
+  });
   server.route(routes);
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
